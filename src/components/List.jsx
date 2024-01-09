78a1141723b1284d96cc302b4f09ba66450a7057
KESHAVsCODE <106483384+KESHAVsCODE@users.noreply.github.com>
2024-01-10 03:06:37 +0530

    implement drag and drop feature

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,29 +1,89 @@
-const List = () => {
+/* eslint-disable react/prop-types */
+import { useEffect, useRef } from "react";
+import { Draggable, Droppable } from "react-beautiful-dnd";
+
+const List = ({ list, setList }) => {
+  const taskInputRef = useRef(null);
+  // const listRef = useRef(null);
+  console.log("----------------------------------------------sstaskList", list);
+
+  const handleCreateTaskClick = (e) => {
+    const taskName = taskInputRef.current.value;
+    if (taskName.trim() === "") return;
+
+    // const newTask = {
+    //   isCompleted: false,
+    //   taskName,
+    // };
+    // setTaskList([...taskList, newTask]);
+    taskInputRef.current.value = "";
+  };
+
+  // useEffect(() => {
+  //   if (listRef.current) {
+  //     const listContainer = listRef.current;
+  //     listContainer.scrollTop = listContainer.scrollHeight;
+  //   }
+  // }, [list]);
+
   return (
     <div className="w-80 border flex flex-col">
       <h3 className="h-[10%] px-4 py-2 text-center font-semibold border-b bg-[#222]">
-        List1
+        {list.listName}
       </h3>
       <div className="h-[90%] px-4 pt-4 flex-grow bg-[#272829]">
-        <div className="pb-4 h-[85%] flex flex-col gap-2 overflow-y-auto">
-          <div className="px-2 py-2 flex items-center gap-2 bg-[#333]">
-            <input type="checkbox" className="w-6 h-6" />
-            <input
-              type="text"
-              disabled
-              className="outline-none bg-[#333] "
-              value="complete work in time for ... complete work in time for ..."
-            />
-          </div>
-        </div>
+        <Droppable droppableId={list.id} type="TASK">
+          {(provided) => (
+            <div
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+              // ref={listRef}
+              className="pb-4 h-[85%] flex flex-col gap-2 "
+            >
+              {list.listItems.map((task, index) => {
+                return (
+                  <Draggable draggableId={task.id} key={task.id} index={index}>
+                    {(provided, snapshot) => (
+                      <div
+                        {...provided.dragHandleProps}
+                        {...provided.draggableProps}
+                        ref={provided.innerRef}
+                        style={{
+                          ...provided.draggableProps.style,
+                          boxShadow: snapshot.isDragging
+                            ? "0 0 10px rgba(0,0,0,0.5)"
+                            : "none",
+                        }}
+                        className="px-4 py-2 flex gap-4 bg-[#333]"
+                      >
+                        <input type="checkbox" className="min-w-8 h-8 border" />
+                        <input
+                          type="text"
+                          disabled
+                          className="w-full outline-none bg-[#333]"
+                          value={task.taskName}
+                        />
+                      </div>
+                    )}
+                  </Draggable>
+                );
+              })}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
         <div className="h-[15%] flex items-center">
           <div className="flex gap-2">
             <input
               type="text"
+              ref={taskInputRef}
               placeholder="List name"
               className="p-2 flex-grow"
             />
-            <button className="w-10 h-10 text-2xl font-semibold  rounded-full cursor-pointer hover:opacity-50 active:scale-90 transition-all bg-gray-500">
+            <button
+              onClick={handleCreateTaskClick}
+              className="w-10 h-10 text-2xl font-semibold  rounded-full cursor-pointer hover:opacity-50 active:scale-90 transition-all bg-gray-500"
+            >
               +
             </button>
           </div>
